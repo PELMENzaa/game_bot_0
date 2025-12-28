26a868e699870734857e11206830c9dfd3dc436a
PELMENzaa <zyukinaa11@gmail.com>
2025-12-28 14:18:16 +0300

    бибабобапдате

--- bibaboba.py
+++ bibaboba.py
@@ -1,25 +1,28 @@
-import logging
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     ContextTypes,
     CommandHandler,
+    MessageHandler,
+    filters,
+    ConversationHandler,
 )
 from dotenv import load_dotenv
 
 import os
 
 async def biba(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.effective_message.text
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="боба",
     )
-    if 'биба' in update.effective_message.text.lower():
+    if 'биба' in text.lower():
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="боба",
         )
-    elif 'боба' in update.effective_message.text.lower():
+    elif 'боба' in text.lower():
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text="биба",
