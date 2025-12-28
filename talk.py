90e8604ec00c2c583bdaed0d7761b87d75ca6179
PELMENzaa <zyukinaa11@gmail.com>
2025-12-28 13:17:21 +0300

    commit

--- talk.py
+++ talk.py
@@ -13,5 +13,16 @@ from dotenv import load_dotenv
 
 import os
 
-async def talk():
-    pass
+async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.effective_message.text
+    if "привет" in text.lower():
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Здарово, брат!",
+        )
+    # чтобы тут обрабатывались 5 рандомных фраз
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=text,
+        )
