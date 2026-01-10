84cb693c2838acfc797799c62bef9798535d024d
PELMENzaa <zyukinaa11@gmail.com>
2026-01-10 18:50:48 +0300

    ошибка с ансвером

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import logging
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     ApplicationBuilder,
     ContextTypes,
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 
 import os
 
-from states import GUESS_NUMBER
+from states import GUESS_NUMBER, MAINMENU
 
 load_dotenv()
 async def game_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -25,8 +25,23 @@ async def game_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id=update.effective_chat.id,
         text= 'так. ну я значит угадываю число, кароче, отвечай, больше, меньше или угадал'
     )
+    keyboard = [['загадал'],['шот я передумал']]
+    markup = ReplyKeyboardMarkup(keyboard)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text= 'число от 1 до 100.',
+        reply_markup=markup
+    )
     return GUESS_NUMBER
-
+    
 async def game(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.effective_message.text
+    if text == 'загадал':
+        pass
+    if text == 'шот я передумал':
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text= 'окей, возвращаемся в главное меню'
+        )
+        return MAINMENU
     
