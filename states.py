5c5bf2c0114611edaba74ee1edba77540ca37490
PELMENzaa <zyukinaa11@gmail.com>
2026-01-31 10:23:01 +0300

    статы почистил

--- states.py
+++ states.py
@@ -1,16 +1,1 @@
-import logging
-from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    ContextTypes,
-    CommandHandler,
-    MessageHandler,
-    filters,
-    ConversationHandler,
-)
-
-from dotenv import load_dotenv
-
-import os
-
 MAINMENU, TALK, BIBA, GUESS_NUMBER, TICTACTOE = range(5)
